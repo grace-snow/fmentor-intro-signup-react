4832ee57738ed4415e86b201bf40271726b8c2bc
Grace Snow <grace.snow@mandogroup.com>
2023-04-17 14:02:54 +0100

    remove unused count state from Vite init

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import LandingPage from "./layout/LandingPage/LandingPage";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
   return (
       <LandingPage />
   );
